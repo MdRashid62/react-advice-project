760fbfcd1d4a97248f5a085746d6f38881972b7f
Muhammad Rashid <55427374+MdRashid62@users.noreply.github.com>
2020-04-23 19:30:20 +0500

    third commit

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
             <div className = "app">
                 <div className = "card">
                     <h1 className = "heading"> { advice } </h1>
-                    <button>
+                    <button className = 'button' onClick={this.fetchAdvice}>
                         <span>GIVE ME ADVICE!</span>
                     </button>
                 </div>
